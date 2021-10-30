18807a2282e5233c9190b197eeeb99b4c58c87d5
nus <buuduc1999@gmail.com>
2021-10-30 22:06:23 +0700

    update X(t) , Y(t), Y(x)

--- fileKetQua.m
+++ fileKetQua.m
@@ -3,26 +3,44 @@ close all
 clear all
 %% cau a
 resultEuler=modifiedEuler()
+
+
 %% cau b
 X=resultEuler.X;
 Y=resultEuler.Y;
 T=resultEuler.T;
-%% lay he so cua spline  
-resultSpline=naturalSpline(X,Y)
-%% tao mot array chua cac ham spline
-listSpline={};
-for i= 1: size(resultSpline,1)
-    listSpline{i}= @(x) resultSpline(i,1)+resultSpline(i,2)*(x-X(i))+resultSpline(i,3)*(x-X(i))^2+resultSpline(i,4)*(x-X(i))^3;
+
+%% 3 splice Y(x)
+resultSplineYx=naturalSpline(X,Y)
+listSplineYx={};
+for i= 1: size(resultSplineYx,1)
+    listSplineYx{i}= @(x) resultSplineYx(i,1)+resultSplineYx(i,2)*(x-X(i))+resultSplineYx(i,3)*(x-X(i))^2+resultSplineYx(i,4)*(x-X(i))^3;
 end
- 
 
+%% 3 splice Y(t)
+resultSplineYt=naturalSpline(T,Y)
+listSplineYt={};
+for i= 1: size(resultSplineYt,1)
+    listSplineYt{i}= @(x) resultSplineYt(i,1)+resultSplineYt(i,2)*(x-X(i))+resultSplineYt(i,3)*(x-X(i))^2+resultSplineYt(i,4)*(x-X(i))^3;
+end
+
+%% 3 splice X(t)
+resultSplineXt=naturalSpline(T,X)
+listSplineXt={};
+for i= 1: size(resultSplineXt,1)
+    listSplineXt{i}= @(x) resultSplineXt(i,1)+resultSplineXt(i,2)*(x-X(i))+resultSplineXt(i,3)*(x-X(i))^2+resultSplineXt(i,4)*(x-X(i))^3;
+end
+%%
 % Kiem tra ket qua spline bac 3 co dung hay k
-% listSpline{2}(2.9551)
-% listSpline{1}(2.9551)
+% listSplineYx{2}(2.9551)
+% listSplineYx{1}(2.9551)
 
-listSpline{3}(5)
+%% Lay so luong con moi tai thang thu 0.5 (0 < 0.5 < 0.625)
+    conMoi0_5=listSplineXt{1}(0.5) % Lay ham splice thu 1 X(t), the vao t=0.5
 
-% Ve do thi
+%% Lay so luong thu san moi vao thang thu 0.8 (0.625 <0.8 < 1.25)   
+    thuSanMoi0_8=listSplineYt{2}(0.8) % Lay ham splice thu 2 Y(t), the vao t=0.8
+%% Ve do thi
 hold on
 plot(T,X,'color','r') 
 plot(T,Y,'color','b')
